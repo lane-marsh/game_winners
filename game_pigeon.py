a43c1aedb75da0eacf3d0ae9a0bb81c5c5cad2f6
Lane Marsh <53096799+lane-marsh@users.noreply.github.com>
2019-08-13 17:01:20 -0700

    Add files via upload

--- game_pigeon.py
+++ game_pigeon.py
@@ -25,6 +25,82 @@ class GamePigeon(object):
         else:
             return False
 
+    def word_hunt(self, letters):
+
+        if len(letters) != 16:
+            return 'error: invalid input'
+
+        letters = letters.upper()
+        letters_array = []
+
+        for letter in letters:
+            letters_array.append(letter)
+
+        moves_dict = {
+            1: [2, 5, 6],
+            2: [1, 3, 5, 6, 7],
+            3: [2, 4, 6, 7, 8],
+            4: [3, 7, 8],
+            5: [1, 2, 6, 9, 10],
+            6: [1, 2, 3, 5, 7, 9, 10, 11],
+            7: [2, 3, 4, 6, 8, 10, 11, 12],
+            8: [3, 4, 7, 11, 12],
+            9: [5, 6, 10, 13, 14],
+            10: [5, 6, 7, 9, 11, 13, 14, 15],
+            11: [6, 7, 8, 10, 12, 14, 15, 16],
+            12: [7, 8, 11, 15, 16],
+            13: [9, 10, 14],
+            14: [9, 10, 11, 13, 15],
+            15: [10, 11, 12, 14, 16],
+            16: [11, 12, 15]
+        }
+
+        moves_list = []
+
+        for start_point in range(16):
+            start = start_point + 1
+
+            temp_combo = [start]
+
+            for move2 in moves_dict[start]:
+                temp_combo.append(move2)
+                for move3 in moves_dict[move2]:
+                    if move3 not in temp_combo:
+                        temp_combo.append(move3)
+                        for move4 in moves_dict[move3]:
+                            if move4 not in temp_combo:
+                                temp_combo.append(move4)
+                                moves_list.append(temp_combo[:])
+                                for move5 in moves_dict[move4]:
+                                    if move5 not in temp_combo:
+                                        temp_combo.append(move5)
+                                        moves_list.append(temp_combo[:])
+                                        for move6 in moves_dict[move5]:
+                                            if move6 not in temp_combo:
+                                                temp_combo.append(move6)
+                                                moves_list.append(temp_combo[:])
+                                                temp_combo.remove(move6)
+                                        temp_combo.remove(move5)
+                                temp_combo.remove(move4)
+                        temp_combo.remove(move3)
+                temp_combo.remove(move2)
+
+        all_words_list = self.gen_words()
+
+        final_list = []
+
+        for move in moves_list:
+            word = ''
+            for position in move:
+                word = word + letters_array[position - 1]
+            if self.check_word(word, all_words_list):
+                final_list.append(word)
+
+        final_list = list(dict.fromkeys(final_list))
+        final_list.sort(key = lambda s: len(s))
+        final_list = final_list[::-1]
+        return final_list
+
     def anagrams(self, letters):
 
         letters = letters.upper()
@@ -54,7 +130,7 @@ class GamePigeon(object):
 
 def test_class(test_string):
     test_game = GamePigeon()
-    return test_game.anagrams(test_string)
+    return test_game.word_hunt(test_string)
 
 
-print(test_class('wordss'))
+print(test_class('iunvtyertnxoeaoa'))
